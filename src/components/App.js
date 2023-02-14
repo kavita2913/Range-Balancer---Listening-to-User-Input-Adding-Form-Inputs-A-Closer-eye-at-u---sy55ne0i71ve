51734acaf7150749935299342c3ea9bd780fdbfc
kavi <116868545+kavita2913@users.noreply.github.com>
2023-02-14 16:31:58 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react'
 import '../styles/App.css';
-import React, { useState } from 'react'
-import '../styles/App.css';
 
 const App = () => {
   const [maxSum, setMaxSum] = useState(50);
